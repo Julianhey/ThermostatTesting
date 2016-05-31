73413dd62a343628ebfdd849705fc9cfc6465e4b
Julianhey <julianhey@hotmail.com>
2016-05-31 12:20:31 +0200

    I dont know  how this works

---
+++ app/src/main/java/nl/tue/thermostattesting/ThermostatActivity.java
@@ -0,0 +1,77 @@
+package nl.tue.thermostattesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class ThermostatActivity extends AppCompatActivity {
+
+    int vtemp = 21;
+    TextView temp;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thermostat);
+        Button bPlus = (Button) findViewById(R.id.bPlus);
+        Button bMinus = (Button) findViewById(R.id.bMinus);
+        temp = (TextView) findViewById(R.id.temp);
+        Button weekOverview = (Button) findViewById(R.id.week_overview);
+
+        weekOverview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(), WeekOverview.class);
+                startActivity(intent);
+            }
+        });
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setProgress(vtemp);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                temp.setText(progress+" \u2103");
+                vtemp =  progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        bPlus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                vtemp ++;
+                temp.setText(vtemp+" \u2103");
+                seekBar.setProgress(vtemp);
+            }
+        });
+        assert bMinus != null;
+        bMinus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                vtemp--;
+                temp.setText(vtemp+" \u2103");
+                seekBar.setProgress(vtemp);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/nl/tue/thermostattesting/WeekOverview.java
@@ -0,0 +1,30 @@
+package nl.tue.thermostattesting;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.widget.ArrayAdapter;
+        import android.widget.Spinner;
+        import android.widget.SpinnerAdapter;
+
+/**
+ * Created by Julian on 23-5-2016.
+ */
+public class WeekOverview extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.week_overview);
+
+        Spinner presetSpinner = (Spinner) findViewById(R.id.presetSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Week_Preset_Names, android.R.layout.simple_spinner_dropdown_item);
+        presetSpinner.setAdapter(adapter);
+
+
+
+    }
+}
