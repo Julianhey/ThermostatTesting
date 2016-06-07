580cc9fa4e461a7e8b818e758eb18c6d2b62a01b
KoenSavelkoul <koensavelkoul@msn.com>
2016-06-07 12:41:59 +0200

    Title bar uitgezet in ThermostatActivity en WeekOverview

--- app/src/main/java/nl/tue/thermostattesting/ThermostatActivity.java
+++ app/src/main/java/nl/tue/thermostattesting/ThermostatActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -21,6 +22,7 @@ public class ThermostatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_thermostat);
         Button bPlus = (Button) findViewById(R.id.bPlus);
         Button bMinus = (Button) findViewById(R.id.bMinus);
--- app/src/main/java/nl/tue/thermostattesting/WeekOverview.java
+++ app/src/main/java/nl/tue/thermostattesting/WeekOverview.java
@@ -4,6 +4,7 @@ package nl.tue.thermostattesting;
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
         import android.support.v7.widget.Toolbar;
+        import android.view.Window;
         import android.widget.ArrayAdapter;
         import android.widget.Spinner;
         import android.widget.SpinnerAdapter;
@@ -20,6 +21,7 @@ public class WeekOverview extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.week_overview);
 
         Spinner presetSpinner = (Spinner) findViewById(R.id.presetSpinner);
